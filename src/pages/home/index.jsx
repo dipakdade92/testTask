bd9b6d54650337ece84eab5c594f9fc65ca3b703
dipakdade92 <dipakdade92@gmail.com>
2024-09-11 21:26:13 +0530

    Mapped all details

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Table, ScrollArea, Badge, useMantineTheme } from "@mantine/core";
+import { Table, ScrollArea, Badge, useMantineTheme, Button } from "@mantine/core";
 import { ContactPageData } from "../../services/apis";
 
 const Home = () => {
@@ -12,10 +12,33 @@ const Home = () => {
       <Table.Td>{company.ownerName || "-"}</Table.Td>
       <Table.Td>{company.email || "-"}</Table.Td>
       <Table.Td>{company.phone || "-"}</Table.Td>
+      <Table.Td>{company.headOffice.address.cityName || "-"}</Table.Td>
+      <Table.Td>{company.headOffice.address.country || "-"}</Table.Td>
+      <Table.Td>{company.headOffice.address.addressLabel || "-"}</Table.Td>
+
       <Table.Td>
         <Badge color={company.rating > 4.5 ? "green" : "yellow"}>
           {company.rating !== undefined ? company.rating : "N/A"}
         </Badge>
+      </Table.Td>
+      <Table.Td>
+        <Button
+        //   onClick={() => handleAdd()}
+          size="xs"
+          variant="outline"
+          color="blue"
+          style={{ marginRight: "10px" }}
+        >
+          Add
+        </Button>
+        <Button
+        //   onClick={() => handleDelete()}
+          size="xs"
+          variant="outline"
+          color="red"
+        >
+          Delete
+        </Button>
       </Table.Td>
     </Table.Tr>
   ));
@@ -56,7 +79,11 @@ const Home = () => {
             <Table.Th>Owner name</Table.Th>
             <Table.Th>Email</Table.Th>
             <Table.Th>Phone</Table.Th>
+            <Table.Th>City</Table.Th>
+            <Table.Th>Country</Table.Th>
+            <Table.Th>Address</Table.Th>
             <Table.Th>Rating</Table.Th>
+            <Table.Th>Actions</Table.Th>
           </Table.Tr>
         </Table.Thead>
         <Table.Tbody>{rows}</Table.Tbody>
