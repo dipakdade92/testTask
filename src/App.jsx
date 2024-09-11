0e799c54bd7be17245efa9aa450243682a630eb4
dipakdade92 <dipakdade92@gmail.com>
2024-09-11 16:24:10 +0530

    Routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import './App.css'
-import '@mantine/core/styles.css';
-import { MantineProvider } from '@mantine/core';
+import "./App.css";
+import "@mantine/core/styles.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+import { MantineProvider } from "@mantine/core";
+import Home from "./pages/home";
+import Login from "./pages/login";
 
 function App() {
   return (
     <MantineProvider>
-      
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </MantineProvider>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/login/index.jsx
@@ -0,0 +1,7 @@
+import { Container } from "@mantine/core";
+
+function Login() {
+  return <Container size={420} my={40}></Container>;
+}
+
+export default Login;
